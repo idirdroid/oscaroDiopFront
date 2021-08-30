39115559a689dca9e829b28721693e8f8aff50a3
Laurent <laurent.rothenbug@gmail.com>
2021-08-30 14:36:27 +0200

    liste de valeurs bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {RouterModule} from "@angular/router";
-import { ModelFilterComponent } from './model-filter/model-filter.component';
-import { ClientListPartComponent } from './client-list-part/client-list-part.component';
-import { ClientComponent } from './client/client.component';
+import {ModelFilterComponent} from './model-filter/model-filter.component';
+import {ClientListPartComponent} from './client-list-part/client-list-part.component';
+import {ClientComponent} from './client/client.component';
 import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {CommonModule} from "@angular/common";
-import { AdminComponent } from './admin/admin.component';
-import { AdminBrandModelComponent } from './admin-brand-model/admin-brand-model.component';
+import {AdminComponent} from './admin/admin.component';
+import {AdminBrandModelComponent} from './admin-brand-model/admin-brand-model.component';
 
 @NgModule({
   declarations: [
@@ -26,14 +26,15 @@ import { AdminBrandModelComponent } from './admin-brand-model/admin-brand-model.
     HttpClientModule,
     BrowserModule,
     ReactiveFormsModule,
-CommonModule,
+    CommonModule,
     RouterModule.forRoot([
       {path: '', component: ClientComponent},
       {path: 'clientlistpart', component: ClientListPartComponent},
       {path: 'adminlistpart', component: AdminComponent}
-      ])
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
